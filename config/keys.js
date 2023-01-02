73452ef9acb5f5b890eebd63cbdf8227a3c73215
Mouni Nischal <d.chiranjeevi007@gmail.com>
2023-01-02 19:06:50 +0530

    Starting a new course since old is outdated

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://SriMouni:Srinidhi5%407@cluster0.9jmkmas.mongodb.net/?retryWrites=true&w=majority",
-};
--- routes/api/posts.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// @route GET api/posts/test
-// @desc Tests the posts route
-// @access Public
-router.get("/test", (req, res) => res.json({ message: "Posts Test API!" }));
-
-module.exports = router;
